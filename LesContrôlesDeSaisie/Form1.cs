5eaa8746d68991f4c1a2263ba3cde9d005c6c0ed
amerlier <salut-yah@yahoo.fr>
2017-10-05 14:03:06 +0200

    LesContrôlesDeSaisie_V1

--- LesContrôlesDeSaisie/Form1.cs
+++ LesContrôlesDeSaisie/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace LesContrôlesDeSaisie
 {
@@ -15,6 +16,93 @@ namespace LesContrôlesDeSaisie
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Valider_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBoxNom.Text + textBoxDate.Text + textBoxMt.Text + textBoxCP.Text);
+        }
+
+        private void Btn_Effacer_Click(object sender, EventArgs e)
+        {
+            textBoxCP.Clear();
+            textBoxDate.Clear();
+            textBoxMt.Clear();
+            textBoxNom.Clear();
+        }
+
+        private void textBoxNom_TextChanged(object sender, EventArgs e)
+        {
+            Regex reg = new Regex("^([a-zA-Z\\s-])+$");
+            if (!reg.IsMatch(textBoxNom.Text))
+            {
+                errorProvider1.SetError(textBoxNom, "Erreur");
+                textBoxNom.BackColor = Color.Red;
+                textBoxNom.Focus();
+            }
+            else
+            {
+                textBoxNom.BackColor = Color.GreenYellow;
+                
+            }
+    
+        }
+
+        private void textBoxDate_TextChanged(object sender, EventArgs e)
+        {
+            Regex reg = new Regex("^([0-9]{2}(/|-)[0-9]{2}(/|-)[0-9]{4})$");
+            DateTime test = new DateTime();
+            if ((!reg.IsMatch(textBoxDate.Text))
+                || (!DateTime.TryParse(textBoxDate.Text, out test))
+                || (DateTime.Today > test)
+                )
+            {
+                errorProvider2.SetError(textBoxDate, "Erreur de format ! Date incorrecte !");
+                textBoxDate.BackColor = Color.Red;
+                textBoxDate.Focus();
+            }
+            else
+            {
+                errorProvider2.Clear();
+                textBoxDate.BackColor = Color.GreenYellow;
+            }
+        }
+
+        private void textBoxMt_TextChanged(object sender, EventArgs e)
+        {
+            Regex reg = new Regex("^([0-9]+)([(\\.|\\,)]?)([0-9]+)$");
+            if (!reg.IsMatch(textBoxMt.Text))
+            {
+                errorProvider3.SetError(textBoxMt, "Montant non valide");
+                textBoxMt.BackColor = Color.Red;
+                textBoxMt.Focus();
+            }
+            else
+            {
+                errorProvider3.Clear();
+                textBoxMt.BackColor = Color.GreenYellow;
+            }
+        }
+
+        private void textBoxCP_TextChanged(object sender, EventArgs e)
+        {
+            Regex reg = new Regex("^([0-9]{5})$");
+            if (!reg.IsMatch(textBoxCP.Text))
+            {
+                errorProvider4.SetError(textBoxCP, "Format du code postal non valide !");
+                textBoxCP.BackColor = Color.Red;
+                textBoxCP.Focus();
+            }
+            else
+            {
+                errorProvider4.Clear();
+                textBoxCP.BackColor = Color.GreenYellow;
+            }
         }
     }
 }
